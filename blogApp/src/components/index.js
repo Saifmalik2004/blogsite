3167a7b1ecc72024f2d0b4411420996ba92c4d93
saifmalik2004 <saifmalik7827.in>
2024-07-09 14:57:52 +0530

    third

--- blogApp/src/components/index.js
+++ blogApp/src/components/index.js
@@ -2,12 +2,30 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./LogoutBtn";
-import Container from "./container";
+import Container from "./Container";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
 
 export {
     Header,
     Footer,
+    Container,
     Logo,
     LogoutBtn,
-    Container
-};
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
+}
